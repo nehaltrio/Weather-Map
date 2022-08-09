8760667719d068d6816e54e827e0b43cd228348f
Farhan-Hasan-Nilok <nilok8679@gmail.com>
2022-08-09 12:02:40 +0600

    Added humidity Air pressure etc

--- JS/App.js
+++ JS/App.js
@@ -1,9 +1,38 @@
 const API_keys = 'e2c35839a8db342617c9c3ccc84027a6';
 //const url = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${API_keys}&units=metric`
 
+
+// function for display search location
 function displayWeather() {
     const search = document.getElementById("search").value;
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${API_keys}&units=metric`)
+        .then(response => response.json())
+        .then(data => {
+
+            // fetching data from API
+            document.querySelector(".temp").innerText = data.main.temp;
+            document.querySelector(".lead").innerText = data.weather[0].description;
+            document.querySelector(".city").innerText = data.name;
+            const url = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`; // weather icon
+            const imgIcon = document.querySelector('.icon');
+            imgIcon.setAttribute('src', url); // setting the icon url in the src attribute of an img tag
+
+            document.querySelector(".humidity").innerText = data.main.humidity;
+            document.querySelector(".pressure").innerText = data.main.pressure;
+            document.querySelector(".wind").innerText = data.wind.speed;
+
+        })
+}
+document.getElementById("button").addEventListener("click", () => {
+
+    displayWeather();
+})
+
+
+// function for display default weather condition 
+
+function defaultCityWeather(city) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_keys}&units=metric`)
         .then(response => response.json())
         .then(data => {
 
@@ -14,9 +43,11 @@ function displayWeather() {
             const imgIcon = document.querySelector('.icon');
             imgIcon.setAttribute('src', url);
 
+            document.querySelector(".humidity").innerText = data.main.humidity;
+            document.querySelector(".pressure").innerText = data.main.pressure;
+            document.querySelector(".wind").innerText = data.wind.speed;
+
         })
 }
-document.getElementById("button").addEventListener("click", () => {
 
-    displayWeather();
-})
+defaultCityWeather("Dhaka");
